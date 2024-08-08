61403a60eb96ce593bf0c965b71dc8c73164bce2
Youssef Shabo <youssefshabo@gmail.com>
2024-08-08 12:51:02 -0500

    fixed the .env problem by using import.meta.env. instead

--- laundryless-website/src/App.jsx
+++ laundryless-website/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -6,7 +7,28 @@ import About from "./pages/About";
 import HowItWorks from "./pages/HowItWorks";
 import Faq from "./pages/Faq";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const App = () => {
+  useEffect(() => {
+    // Your web app's Firebase configuration using environment variables
+    const firebaseConfig = {
+      apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+      storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: import.meta.env.VITE_FIREBASE_APP_ID,
+      measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }, []);
+
   return (
     <Router>
       <Navbar />
